60052e6fae16b923ba7f0ab07d74843457a0c95d
aety <anneyanget@gmail.com>
2020-08-16 16:13:45 -0500

    formatting

--- preprocess.py
+++ preprocess.py
@@ -18,37 +18,49 @@ data = data.rename(columns={'Year-we-met': 'Year met', 'Date': 'Reply date'})
 # convert to datetime
 data['Reply date'] = pd.to_datetime(data['Reply date'])
 
-
-
-# plot counts
-columns = ['Source', 'Country', 'Language', 'Year met', 'Reply date']
-for n, column in enumerate(columns):
-    print(column)
-    plt.figure(figsize=(6, 4))
-    values = data.groupby(column).count()['Name']
-    plt.bar(values.index, values, facecolor='0.25', edgecolor=None)
-    plt.xticks(rotation=45)
-    plt.xlabel(column)
-    plt.tight_layout()
-    plt.savefig('frequency_' + str(n) + '.jpg')
-    plt.close()    
-
-
 # count lengths of texts
 df = pd.DataFrame()
 breakdown = ['greetings', 'story', 'advice', 'others']
 for number, column in enumerate(breakdown):
     one_series = data['Breakdown-' + column].fillna('')
     data.loc[:, column] = one_series.apply(len)
-    data.loc[:, 'Index'] = data.index
-    df_sub = data.loc[:, ['Index', column]]
+    data.loc[:, 'Reply'] = data.index
+    df_sub = data.loc[:, ['Reply', column]]
     df_sub.loc[:, 'length'] = df_sub.loc[:, column]
     df_sub.loc[:, 'theme'] = [column]*(len(df_sub))
-    df_sub = df_sub.loc[:, ['Index', 'length', 'theme']]
+    df_sub = df_sub.loc[:, ['Reply', 'length', 'theme']]
     df = df.append(df_sub)
 
-plt.figure(figsize=(5, 4))
-sns.catplot(data=df, x='Index', y='length', hue='theme', kind='bar', legend_out=False)
-#plt.title('Length of text by theme, per person')
+plt.figure(figsize=(10, 3))
+g = sns.barplot(data=df, x='Reply', y='length', hue='theme')
+g.legend(loc='best', ncol=4)
+plt.tight_layout()
 plt.savefig('lengths.jpg')
 plt.close()
+
+# plot counts
+data['Total length'] = data['greetings'] + data['story'] + data['advice'] + data['others']
+columns = ['Source', 'Country', 'Language', 'Year met', 'Reply date']
+for n, column in enumerate(columns):
+    print(column)
+    plt.figure(figsize=(5, 3))
+    values = data.groupby(column).count()['Name']
+    plt.bar(values.index, values, facecolor='0.25', edgecolor='0.25')
+    plt.xticks(rotation=45)
+    plt.xlabel(column)
+    plt.tight_layout()
+    plt.savefig('frequency_' + str(n) + '.jpg')
+    plt.close()    
+
+# plot total text lengths
+column = 'Total length'
+plt.figure(figsize=(5, 3))
+values = data[column]
+plt.hist(values, bins=len(values), color='0.25')
+plt.xlabel(column)
+plt.tight_layout()
+plt.savefig('hist.jpg')
+plt.close()
+
+data.to_json('AY_30_proc.json')
+
